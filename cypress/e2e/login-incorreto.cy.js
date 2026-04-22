d6d5b89e4850772a101acc2725e1dd3a4e5455bd
Luis Parente <parente.lgsp@outlook.com>
2026-04-22 17:31:52 -0300

    Testes da página de login

---
+++ cypress/e2e/login-incorreto.cy.js
@@ -0,0 +1,34 @@
+describe('Página de login', () => {
+    beforeEach(() => {
+        cy.visit('https://adopet-frontend-cypress.vercel.app/');
+        cy.get('[data-test="login-button"]').click();
+    })
+
+    it('Deve deixar os campos de email e senha em branco e exibir mensagem de erro', () => {
+        cy.get('[data-test="submit-button"]').click();
+        cy.contains('É necessário informar um endereço de email').should('be.visible');
+        cy.contains('Insira sua senha').should('be.visible');
+    })
+
+    it('Deve preencher o campo de email incorretamente e exibir mensagem de erro', () => {
+        cy.get('[data-test="input-loginEmail"]').type('ana.exemplo.123');
+        cy.get('[data-test="input-loginPassword"]').type('Ana@123');
+        cy.get('[data-test="submit-button"]').click();
+        cy.contains('Por favor, verifique o email digitado').should('be.visible');
+    })
+
+    it('Deve preencher o campo de senha incorretamente e exibir mensagem de erro', () => {
+        cy.get('[data-test="input-loginEmail"]').type('ana.exemplo.123@exemplo.com');
+        cy.get('[data-test="input-loginPassword"]').type('ana@123');
+        cy.get('[data-test="submit-button"]').click();
+        cy.contains('A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres').should('be.visible');
+    })
+
+    it('Deve preencher os campos de email e senha incorretamente e exibir mensagens de erro', () => {
+        cy.get('[data-test="input-loginEmail"]').type('ana.exemplo.123');
+        cy.get('[data-test="input-loginPassword"]').type('ana@123');
+        cy.get('[data-test="submit-button"]').click();
+        cy.contains('Por favor, verifique o email digitado').should('be.visible');
+        cy.contains('A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres').should('be.visible');
+    })
+})
