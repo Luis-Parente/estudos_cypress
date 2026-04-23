ea4fc80d8832d05920fcf762517810c93a46f119
Luis Parente <parente.lgsp@outlook.com>
2026-04-23 19:38:23 -0300

    Corrigida segurança de token da API

--- cypress/e2e/api-perfil.cy.js
+++ cypress/e2e/api-perfil.cy.js
@@ -1,11 +1,9 @@
 describe('API do Adopet', () => {
-    const auth = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTc3Njk4MTc1NSwiZXhwIjoxNzc3MjQwOTU1fQ.M9-yre0SYd4HXgr4UPK3gdhBKKVhxpf6mLjPpXjZX5w`;
-
     it('Deve retornar nome do usuário autenticado', () => {
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/11643cd6-7112-415b-95d2-07904b0d1a1c',
-            headers: { Authorization: auth }
+            headers: Cypress.env()
         }).then((res) => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
